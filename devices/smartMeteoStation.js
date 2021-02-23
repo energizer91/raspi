c149fd120b470d8f1678a1f95b5c9743642dc66a
Alexander Bareyko <militaryboy@yandex.ru>
2021-02-23 21:54:00 +0100

    fix meteo station name

--- devices/smartMeteoStation.js
+++ devices/smartMeteoStation.js
@@ -4,7 +4,7 @@ class SmartMeteoStation extends SmartThermometer {
   constructor(uid, smartHub, config) {
     super(uid, smartHub, config);
 
-    this.model = 'smartMeteoStation';
+    this.model = 'SmartMeteoStation';
     this.name = 'Smart meteo station';
   }
 }
